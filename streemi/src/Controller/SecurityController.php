f5ea411c580a163d7c5fb33d2f40652a43d982ac
Lydia-BEDRI <bedrilydia118@gmail.com>
2024-12-14 00:43:58 +0100

    set up the login page

--- streemi/src/Controller/SecurityController.php
+++ streemi/src/Controller/SecurityController.php
@@ -18,7 +18,7 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastEmail= $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', [
+        return $this->render('auth/login.html.twig', [
             'last_username' => $lastEmail,
             'error' => $error,
         ]);
@@ -28,5 +28,29 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/register', name: 'page_register')]
+    public function register(): Response
+    {
+        return $this->render('auth/register.html.twig');
+    }
+
+    #[Route(path: '/forgot', name: 'page_forgot_password')]
+    public function forgotPassword(): Response
+    {
+        return $this->render('auth/forgot.html.twig');
+    }
+
+    #[Route(path: '/reset', name: 'page_reset_password')]
+    public function resetPassword(): Response
+    {
+        return $this->render('auth/reset.html.twig');
+    }
+
+    #[Route(path: '/confirm', name: 'page_confirm_account')]
+    public function confirmAccount(): Response
+    {
+        return $this->render('auth/confirm.html.twig');
     }
 }
